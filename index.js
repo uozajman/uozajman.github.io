c80ed984036535b1f0190093cd9c8ecff588b666
uozajman <ytteamsac716@gmail.com>
2026-01-25 18:28:31 +0900

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('.hamburger').click(function() {
-    // メニューの開閉状態を切り替える
-    $('.menu').toggleClass('open');
-
-    // ハンバーガーボタンのアクティブクラスを切り替えて三本線をバツにする
-    $(this).toggleClass('active');
-  });
-});
